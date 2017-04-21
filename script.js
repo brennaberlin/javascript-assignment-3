a64c2100818a2a6d63557d55e3ecc808891ee52b
Brenna Berlin <brenna@id.iit.edu>
2017-04-20 21:37:51 -0500

    New format for var

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 // Do your work in this file.
 
-var div_view = document.createElement("div_view");
-div_view.className = "div_view";
+var view = document.createElement("div_view");
+view.className = "div_view";
 document.body.appendChild(div_view);
 
-var div_grass = "<div class='grass'></div>";
-document.getElementsByClassName("view").appendChild(div_grass);
+var div_grass = document.createElement("grass");
+grass.className = "grass";
+document.body.appendChild(view);
 
-var div_sun = "<div class='sun'></div>";
-document.getElementsByClassName("grass").appendChild(div_sun);
+var sun = document.createElement("sun");
+sun.className = "sun";
+doument.body.appendChild(view);
+
+var mountain = document.createElement("mountain");
+mountain.className = "mountain";
+document.body.appendChild(view);
+
+var mountain-top = document.createElement("mountain-top");
+mountain-top.className = "mountain-top";
+document.body.appendChild(mountain)
+
+
+// var div_grass = "<div class='grass'></div>";
+// document.getElementsByClassName("view").appendChild(div_grass);
+
+// var div_sun = "<div class='sun'></div>";
+// document.getElementsByClassName("grass").appendChild(div_sun);
 
 var i = 0;
 while (i < 5) {
@@ -17,11 +34,11 @@ while (i < 5) {
     i++;
 }
 
-var div_mountain = "<div class='mountain'></div>";
-document.getElementsByClassName("view").appendChild(div_mountain);
+// var div_mountain = "<div class='mountain'></div>";
+// document.getElementsByClassName("view").appendChild(div_mountain);
 
-var div_mountaintop = "<div class='mountain-top'></div>";
-document.getElementsByClassName("mountain").appendChild(div_mountaintop);
+// var div_mountaintop = "<div class='mountain-top'></div>";
+// document.getElementsByClassName("mountain").appendChild(div_mountaintop);
 
 var k = 0;
 do {
